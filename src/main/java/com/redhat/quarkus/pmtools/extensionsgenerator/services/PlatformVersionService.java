710316f80869afaf4478a074413a2928a83768d8
Thomas Qvarnström <tqvarnst@redhat.com>
2023-02-23 15:56:41 +0100

    Filtered version based on short name and select the one with the largest build number.

--- src/main/java/com/redhat/quarkus/pmtools/extensionsgenerator/services/PlatformVersionService.java
+++ src/main/java/com/redhat/quarkus/pmtools/extensionsgenerator/services/PlatformVersionService.java
@@ -1,11 +1,14 @@
 package com.redhat.quarkus.pmtools.extensionsgenerator.services;
 
 import com.redhat.quarkus.pmtools.extensionsgenerator.models.Platform;
+import com.redhat.quarkus.pmtools.extensionsgenerator.utils.VersionUtils;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ApplicationScoped
 public class PlatformVersionService {
@@ -19,7 +22,32 @@ public class PlatformVersionService {
 
         platforms.forEach(p -> p.getStreams().forEach(s -> s.getReleases().forEach(r ->  versions.add(r.getVersion()))));
 
-        return versions;
+        return filterPlatformVersionsByShortNames(versions);
+    }
+
+    private List<String> filterPlatformVersionsByShortNames(List<String> versionList) {
+        Map<String,String> shortVersionMap = new HashMap<>();
+
+        versionList.forEach( v -> {
+            String shortVersion = VersionUtils.shortVersion(v);
+            if(shortVersionMap.containsKey(shortVersion)) {
+                if(largerThanExistingVersion(v,shortVersionMap.get(shortVersion))) {
+                    shortVersionMap.replace(shortVersion,v);
+                }
+            } else {
+                shortVersionMap.put(shortVersion,v);
+            }
+        });
+
+        return new ArrayList<>(shortVersionMap.values());
+
+    }
+
+    private boolean largerThanExistingVersion(String v, String s) {
+        String shortName = VersionUtils.shortVersion(v);
+        String vBuildNumber = v.replace(shortName+"-redhat-","");
+        String sBuildNumber = s.replace(shortName+"-redhat-","");
+        return Integer.parseInt(vBuildNumber) > Integer.parseInt(sBuildNumber);
     }
 }
 
